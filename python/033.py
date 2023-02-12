d2213e27fbaab87161f5d38010b8a12c73f19ba3
Felipe Tadeu <souzalima@gmail.com>
2023-02-12 11:12:53 -0300

    033 done

--- python/033.py
+++ python/033.py
@@ -4,22 +4,30 @@ def gcd(a, b):
     return a
 
 
-def simplify_fraction(n, d):
-    g = gcd(n, d)
-    return n // g, d // g
+def is_cancelling_fraction(n, d):
+    n_digits = [int(x) for x in str(n)]
+    d_digits = [int(x) for x in str(d)]
+    common = set(n_digits) & set(d_digits)
+    if len(common) != 1:
+        return False
+    digit = common.pop()
+    if digit == 0:
+        return False
+    n_digits.remove(digit)
+    d_digits.remove(digit)
+    if d_digits[0] == 0:
+        return False
+    reduced_n = n_digits[0]
+    reduced_d = d_digits[0]
+    return n / d == reduced_n / reduced_d
 
 
-def find_denominator():
-    numerator_product = 1
-    denominator_product = 1
-    for denominator in range(11, 100):
-        for numerator in range(10, denominator):
-            n1, d1 = simplify_fraction(numerator, denominator)
-            n2, d2 = simplify_fraction(numerator % 10, denominator % 10)
-            if numerator // 10 == denominator // 10 and n1 == n2 and d1 == d2:
-                numerator_product *= numerator
-                denominator_product *= denominator
-    return denominator_product // gcd(numerator_product, denominator_product)
+numerator_product = 1
+denominator_product = 1
+for numerator in range(10, 100):
+    for denominator in range(numerator + 1, 100):
+        if is_cancelling_fraction(numerator, denominator):
+            numerator_product *= numerator
+            denominator_product *= denominator
 
-
-print(find_denominator())
+print(denominator_product // gcd(numerator_product, denominator_product))
