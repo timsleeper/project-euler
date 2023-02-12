3a9c25790a1dc219d20bee6a0c84d4ac5e4d2bcd
Felipe Tadeu <souzalima@gmail.com>
2023-02-12 09:12:17 -0300

    refactoring 012

--- python/021.py
+++ python/021.py
@@ -1,20 +1,23 @@
-def sum_factors(n):  
-     result = []
-     for i in range(1, int(n**0.5) + 1):
-         if n % i == 0:
-             result.extend([i, n//i])
-     return sum(set(result)-set([n]))
+def proper_divisors(n):
+    divisors = []
+    for i in range(1, n):
+        if n % i == 0:
+            divisors.append(i)
+    return divisors
 
-def amicable_pair(number):
-    result = []
-    for x in range(1,number+1):
-        y = sum_factors(x)
-        if sum_factors(y) == x and x != y:
-            result.append(tuple(sorted((x,y))))
-    return set(result)
 
-res = 0
-for i in amicable_pair(10000):
-    res = res + i[0] + i[1]
+def is_amicable(n):
+    m = sum(proper_divisors(n))
+    return n != m and n == sum(proper_divisors(m))
 
-print(res)
+
+def sum_of_amicable_numbers(limit):
+    amicable_numbers = []
+    for i in range(1, limit):
+        if is_amicable(i) and i not in amicable_numbers:
+            amicable_numbers.append(i)
+            amicable_numbers.append(sum(proper_divisors(i)))
+    return sum(amicable_numbers)
+
+
+print(sum_of_amicable_numbers(10000))
