7a852b4c6146f4ca5ea00c95eef5137928bf92de
Felipe Tadeu <souzalima@gmail.com>
2023-03-16 20:33:52 -0300

    035 done

--- python/035.py
+++ python/035.py
@@ -1,23 +1,33 @@
 import itertools
 
-
 def is_prime(n):
-    if n < 2:
+    if n <= 1:
         return False
-    for i in range(2, int(n**0.5) + 1):
-        if n % i == 0:
+    if n <= 3:
+        return True
+    if n % 2 == 0 or n % 3 == 0:
+        return False
+    i = 5
+    while i * i <= n:
+        if n % i == 0 or n % (i + 2) == 0:
             return False
+        i += 6
     return True
 
+def get_rotations(n):
+    str_n = str(n)
+    return [int(str_n[i:] + str_n[:i]) for i in range(len(str_n))]
 
 def is_circular_prime(n):
-    str_n = str(n)
-    rotations = [int("".join(p)) for p in itertools.permutations(str_n)]
-    return all(is_prime(r) for r in rotations)
+    return all(is_prime(rotation) for rotation in get_rotations(n))
 
+def count_circular_primes(limit):
+    count = 0
+    for num in range(2, limit):
+        if is_circular_prime(num):
+            count += 1
+    return count
 
-def find_circular_primes():
-    return sum(1 for i in range(2, 1000000) if is_circular_prime(i))
-
-
-print(find_circular_primes())
+limit = 1000000
+circular_primes_count = count_circular_primes(limit)
+print("There are {} circular primes below one million.".format(circular_primes_count))
